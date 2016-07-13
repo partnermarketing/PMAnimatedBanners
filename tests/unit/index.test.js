2247019668bd2ef13986ed2168bb73c2f3dd6601
Simon Staton <simon.staton@live.co.uk>
2016-07-13 17:24:51 +0100

    Removing index.js test as it is no longer required

--- tests/unit/index.test.js
+++
@@ -1,8 +0,0 @@
-import index from '../../src/index';
-import Loader from '../../src/loader';
-
-describe('index', function () {
-  it('should instantiate and return a loader', ()=>{
-    expect(index instanceof Loader).toEqual(true);
-  });
-});
